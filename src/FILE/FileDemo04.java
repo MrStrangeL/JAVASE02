3f26d415a3f211b7cb299170c7ea569f7577b222
liuyangbing <1832567496@qq.com>
2016-09-27 20:42:54 +0800

    add github address

---
+++ src/FILE/FileDemo04.java
@@ -0,0 +1,35 @@
+package FILE;
+
+import java.io.File;
+
+public class FileDemo04 {
+
+	/**
+	 * 使用递归来删除文件或目录
+	 * @param file
+	 */
+	public static void deleteFile(File file){
+		if(file.isDirectory()){
+			//如果是目录还要看是否还有子项
+			File[] direcctoryArray=file.listFiles();
+			//先删除所有子项
+			for (File newdirecctory : direcctoryArray) {
+				deleteFile(newdirecctory);
+			}
+		}
+		file.delete();
+	}
+	
+	/**
+	 * 使用递归来累加数据
+	 * @param n
+	 * @return
+	 */
+	public static int add(int n){
+		if(n==1){
+			return 1;
+		}else{
+			return n+add(n-1);
+		}
+	}
+}
---
+++ src/FILE/RandomAccessFileDemo03.java
@@ -0,0 +1,45 @@
+package FILE;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+/**
+ * 查看RandomAcessFile的指针位置
+ * RAF总是根据当前指针所指向的位置进行读写操作，并且每次读写后，指针自动向后移动
+ * @author Administrator
+ *
+ */
+public class RandomAccessFileDemo03 {
+	public static void main(String[] args){
+		try {
+			RandomAccessFile raf=new RandomAccessFile("demo.t", "rw");
+			System.out.println(raf.getFilePointer());//0
+			raf.write(97);
+			System.out.println(raf.getFilePointer());//1
+			
+			raf.writeInt(990);
+			System.out.println(raf.getFilePointer());//5
+			
+			/**
+			 * 此时指针是向5，所以返回的值为-1(在文件的末尾)
+			 */
+			int x=raf.read();
+			System.out.println(x+","+raf.getFilePointer());
+			
+			//将文件的指针移动到文件的开头
+			raf.seek(0);
+			System.out.println(raf.read()+","+raf.getFilePointer());
+			System.out.println(raf.readInt()+","+raf.getFilePointer());
+			
+			
+			/**
+			 * int skipBytes(int n)
+			 * 该方法会尝试跳过n个字节，返回值为实际跳过的字节数，只能往后跳
+			 */
+			raf.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
