8cfe18c0abc612ffa3ab2c07f9d8f566feca247a
liuyangbing <1832567496@qq.com>
2016-09-27 21:42:27 +0800

    FIS and FOS about file

---
+++ src/FILE/FISDemo01.java
@@ -0,0 +1,5 @@
+package FILE;
+
+public class FISDemo01 {
+
+}
---
+++ src/FILE/FOSDemo01.java
@@ -0,0 +1,18 @@
+package FILE;
+
+/**
+ * Inputstream:输入流(输入流是用来读的)---->>读入
+ * Outputstream:输出流(输出流是用来写的)---->>写出
+ * Inputstream和Outputstream是抽象类，收所有字节输入流和字节输出流的父类
+ */
+
+/**
+ * FileOutputStream:节点流(低级流)，向文件中写出数据 
+ * @author Administrator
+ *
+ */
+public class FOSDemo01 {
+	public static void main(String[] args){
+		
+	}
+}
